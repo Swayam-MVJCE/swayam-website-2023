332644bd4cfb8f22293994bbb451bb9f2ae921cc
Varun Panyam <2003varunp@gmail.com>
2023-11-19 00:16:10 +0530

    tweaks

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -16,6 +16,7 @@ import getUuid from "uuid-by-string";
 import { useRouter } from "next/router";
 
 const GOLDENRATIO = 1.61803398875;
+//
 
 export default function Events({ images }) {
   return (
